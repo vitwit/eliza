5f137ac68f1b16d5cc519508311822bd6162f314
anilcse <anil@vitwit.com>
2025-01-07 22:19:36 +0530

    fix merge conflicts

--- packages/plugin-cosmos/src/index.ts
+++ packages/plugin-cosmos/src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Plugin } from "@elizaos/core";
 import { walletProvider } from "./providers/wallet";
 import executeTransfer from "./actions/transfer";
@@ -9,19 +8,6 @@ export const cosmosPlugin: Plugin = {
     providers: [walletProvider],
     actions: [executeTransfer],
     evaluators: [],
-=======
-import { cosmosWalletProvider } from "./providers/wallet.ts";
-import type { Plugin } from "@elizaos/core";
-import { balanceAction } from "./actions/walletProviderTestAction.ts";
-
-export const cosmosPlugin: Plugin = {
-    name: "cosmos",
-    description: "Cosmos blockchain integration plugin",
-    providers: [cosmosWalletProvider],
-    evaluators: [],
-    services: [],
-    actions: [balanceAction],
->>>>>>> 49d76c39f6bebe8ae00b936817fca474dfc3e2d4
 };
 
 export default cosmosPlugin;
